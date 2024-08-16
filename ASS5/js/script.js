a32e19675d15d91b28d35b65cc0b5b9decaee20c
VictorCastro <160255120+TYCCUS@users.noreply.github.com>
2024-08-16 17:47:56 +0200

    fixed bug

--- ASS5/js/script.js
+++ ASS5/js/script.js
@@ -66,7 +66,12 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(allCategoriesUrl,buildAndShowHomeHTML,true);
 // Explicitly setting the flag to get JSON from server processed into an object literal
 });
-
+/*
+|
+|
+|ASSIGNMENT
+|
+*/
 // Builds HTML for the home page based on categories array returned from the server.
 function buildAndShowHomeHTML (categories) {
   // Load home snippet page
