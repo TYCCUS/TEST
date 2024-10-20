622f981dffa2957fbbe7817e023b58495a3a1708
VictorCastro <160255120+TYCCUS@users.noreply.github.com>
2024-10-20 03:43:58 +0200

    constructor function

---
+++ ConstructorFunction/index.js
@@ -0,0 +1,26 @@
+/*
+Challenge:
+1. Create a constructor function called 'Character'.
+2. Give it 'name' and 'collectedItemsArr' properties. 
+    - 'name' should hold the character’s name.
+	- 'collectedItemsArr' should hold an array of items. 
+       Initialise it to an empty array.
+3. Create an 'addItem' method which takes in an item as 
+   a parameter and adds it to 'collectedItemsArr'.
+4. The addItem method should log out a sentence like 
+   `Merlin now has: wand, map, potion`.
+5. Check it’s working by creating several instances of 
+   Character and adding items to their arrays.
+*/
+function Character(name,collectedItemsArr=[]){
+   this.name = name;
+   this.collectedItemsArr = [];
+   this.addItem = (item) => {
+      this.collectedItemsArr.push(item);
+      console.log(`${this.name} now has ${this.collectedItemsArr.toString()}`)
+   }
+}
+
+const merlin = new Character('Merlin');
+merlin.addItem('spoon');
+merlin.addItem('cauldron');
