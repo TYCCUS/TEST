3e3e7232a1180ab3782330aa9ef305d8a531d1a1
VictorCastro <160255120+TYCCUS@users.noreply.github.com>
2024-10-22 23:10:38 +0200

    Find employees recursion

--- FindEmployeeByIdRecursion/index.js
+++ FindEmployeeByIdRecursion/index.js
@@ -1,33 +1,32 @@
 // import {organizationData} from './orgData.js'
-import {organizationData} from './orgData'
-console.log(organizationData)
-// // function searchEmployeeById() {
-// // /*
-// // Challenge:
-// // 1. Write a function that searches for an employee in 'organizationData'. The function should recursively traverse the nested objects and find all employees with a specified ID. 
+import {organizationData} from './orgData.js'
+// console.log(organizationData)
+// function searchEmployeeById() {
+// /*
+// Challenge:
+// 1. Write a function that searches for an employee in 'organizationData'. The function should recursively traverse the nested objects and find all employees with a specified ID. 
 
-// // Stretch Goal:
-// // 💪 Complete the challenge without declaring any variable in the global scope.
-// // */
+// Stretch Goal:
+// 💪 Complete the challenge without declaring any variable in the global scope.
+// */
 
-// // }
-// // const employee = searchEmployeeById(organizationData, 5) // pass in data and employee id.
+// }
+// const employee = searchEmployeeById(organizationData, 5) // pass in data and employee id.
 
-// const searchEmployeeById = obj => id => {
-//     for (let key in obj) {
-//       if (typeof obj[key] === 'object') {
-//         const result = findEmployeeNameById(obj[key], id);
-//         if (result) {
-//           return result;
-//         }
-//       } else if (key === 'id' && obj[key] === id) {
-//         return obj.name;
-//       }
-//     }
-//     return null; 
-//   }
+const searchEmployeeById = obj => id => {
+    for (let key in obj) {
+      if (typeof obj[key] === 'object') {
+        const result = searchEmployeeById(obj[key])(id) ;
+        if (result) {
+          return result;
+        }
+      } else if (key === 'id' && obj[key] === id) {
+        return `obj.name | position: ${obj.position}`;
+      }
+    }
+    return null; 
+  }
 
-// const oData = searchEmployeeById(organizationData)
-// const employee = oData(5)
+const employeeData = searchEmployeeById(organizationData)
 
-// console.log(employee) // Output: [{id: 5, name: "Kwame Mensah", position: "Sales Executive"}]
+console.log(employeeData(5)) // Output: [{id: 5, name: "Kwame Mensah", position: "Sales Executive"}]
