23a65941120355ac2abc777a61a0fb34b9252b19
VictorCastro <160255120+TYCCUS@users.noreply.github.com>
2024-10-23 19:17:57 +0200

    table devEnv

--- FindEmployeeByIdRecursion/index.js
+++ FindEmployeeByIdRecursion/index.js
@@ -30,3 +30,27 @@ const searchEmployeeById = obj => id => {
 const employeeData = searchEmployeeById(organizationData)
 
 console.log(employeeData(5)) // Output: [{id: 5, name: "Kwame Mensah", position: "Sales Executive"}]
+
+// alternative
+
+// function searchEmployeeById(data, targetId) {
+//     let results = []
+//     for (let key in data) {
+//         if (key === 'employees') {
+//             for (let item of data[key]) {
+//                if (item.id === targetId) {
+//                    results.push(item)
+//                } 
+//             }
+//         } else if (typeof data[key] === 'object') {
+//             results = [...results, ...searchEmployeeById(data[key], targetId)]
+//         }
+//     }
+//     return results
+// }
+
+// const employee = searchEmployeeById(organizationData, 5) // pass in data and employee id.
+// const employee2 = searchEmployeeById(organizationData, 6) // pass in data and employee id.
+
+// console.log('employee: ', employee)
+// console.log('employee2: ', employee2)
---
+++ Table/index.js
@@ -0,0 +1,2 @@
+import {data} from './testData'
+console.log(data)
