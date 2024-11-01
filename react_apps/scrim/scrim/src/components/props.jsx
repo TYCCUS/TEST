0539604c420f53a9f57e83f00db9e3a6191ac38d
VictorCastro <160255120+TYCCUS@users.noreply.github.com>
2024-11-01 17:37:59 +0100

    vCard new

--- react_apps/scrim/scrim/src/components/props.jsx
+++ react_apps/scrim/scrim/src/components/props.jsx
@@ -9,7 +9,7 @@ export const pageProps = {
     navMenuItems: {className: 'd-flex flex-row justify-content-end me-5', style: { listStyleType: 'none' }},
     menuItems: {className:'mx-2 cursor-pointer'},
     mainSection: {className:'d-flex flex-column justify-content-start align-items-stretch flex-grow-1'},
-    mainTitle:{className:'TXT-HLX-MED txt-display TXT-PRIMARY text-center mt-5'},
+    mainTitle:{className:'TXT-HLX-BOLD txt-display TXT-PRIMARY text-center mt-5 uppercase'},
     footer:{className:'H50PX BG-SECONDARY ps-5 flex-grow-0 relative'},
     footerText:{className: 'LEFT-CENTER TXT-HLX-LIGHT txt-xs TXT-DARK'},
 }
